60d41d987defd7ad84bd795974592a252bef8dd7
Jakub <jacob98k@hotmail.com>
2021-09-13 00:19:01 +0200

    dodawanaie użytkowników

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,6 +71,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'created_at' => null,
+            'updated_at' => null,
         ]);
     }
 
