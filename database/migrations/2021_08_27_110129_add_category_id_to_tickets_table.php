cde84a2ef55fcbf9db1ce859d95792ac00c1d90e
Jakub <jacob98k@hotmail.com>
2021-09-21 01:57:39 +0200

    soft delete ticketow przy okazji usuwania uzytkownika

--- database/migrations/2021_08_27_110129_add_category_id_to_tickets_table.php
+++ database/migrations/2021_08_27_110129_add_category_id_to_tickets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCategoryIdToTicketTable extends Migration
+class AddCategoryIdToTicketsTable extends Migration
 {
     /**
      * Run the migrations.
